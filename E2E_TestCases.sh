5a0405065747d09ee2bfb47f203d8883f1fcb7c9
hivijayreddy <97868617+hivijayreddy@users.noreply.github.com>
2022-03-16 08:11:54 +0530

    Update E2E_TestCases.sh

--- E2E_TestCases.sh
+++ E2E_TestCases.sh
@@ -6,6 +6,7 @@ gcloud config set account 'vijayabh@google.com'
 gcloud config configurations activate default
 gcloud projects list
 gcloud config set project dataplex-cdf
+gcloud auth application-default login
 
 #Environment
 STAGING=https://staging-dataplex.sandbox.googleapis.com/
